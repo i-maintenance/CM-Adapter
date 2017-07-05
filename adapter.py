160e4e6646002e836224a7683bddc68bcfccba8c
Johannes Innerbichler <johannes.innerbichler@salzburgresearch.at>
2017-07-05 13:02:31 +0200

    fixed minor bug in fetching of sensor data

--- adapter.py
+++ adapter.py
@@ -9,8 +9,8 @@ from kafka import KafkaProducer
 
 CM_APP_HOST = 'http://192.168.13.101'
 KAFKA_TOPIC = 'SensorData'
-BOOTSTRAP_SERVERS = ['il061:9092']
-UPDATE_INTERVAL = 2  # in minutes
+BOOTSTRAP_SERVERS = ['il061:9092', 'il062:9092', 'il063:9092']
+UPDATE_INTERVAL = 1  # in minutes
 
 producer = KafkaProducer(bootstrap_servers=BOOTSTRAP_SERVERS,
                          api_version=(0, 9),
@@ -22,19 +22,25 @@ logger = logging.getLogger(__name__)
 
 def update(last_sent_time=None):
     try:
+        # fetch sensor data
         sensor_data = fetch_sensor_data()
         logger.info('Fetched {} sensor entries'.format(len(sensor_data)))
-        last_sent_time, num_entries_sent = publish_sensor_data(sensor_data=sensor_data,
-                                                               last_sent_time=last_sent_time)
-        logger.info('Published {} new sensor entries till {}'.format(num_entries_sent, last_sent_time))
+
+        # filter data
+        data_to_send = sensor_data.ix[sensor_data.index > last_sent_time] if last_sent_time else sensor_data
+
+        # delegate to messaging bus
+        publish_sensor_data(sensor_data=data_to_send)
+        last_sent_time = sensor_data.index[-1]
+        logger.info('Published {} new sensor entries till {}'.format(len(data_to_send), last_sent_time))
+
     except Exception as e:
         logger.exception(e)
 
     # schedule next update
     next_time = datetime.now() + timedelta(minutes=UPDATE_INTERVAL)
-    threading.Timer((next_time - datetime.now()).total_seconds(),
-                    function=update,
-                    kwargs={'last_sent_time': last_sent_time}).start()
+    interval = (next_time - datetime.now()).total_seconds()
+    threading.Timer(interval=interval, function=update, kwargs={'last_sent_time': last_sent_time}).start()
     logger.info('Scheduled next update at {}'.format(next_time))
 
 
@@ -47,12 +53,12 @@ def fetch_sensor_data():
 
     # read sensor data from csv file
     csv_reader = csv.reader(csv_data)
-    csv_header = next(csv_reader)
+    csv_header = next(csv_reader)  # read header information
 
     sensor_data = pd.DataFrame(columns=csv_header)
     for row in csv_reader:
         if row:  # due to blank line at the bottom
-            sensor_data.loc[row[0]] = list(row)
+            sensor_data.loc[row[0]] = list(row)  # index is first field (i.e. 'Result')
 
     # convert timestamp
     sensor_data['Zeitstempel'] = pd.to_datetime(sensor_data['Zeitstempel'])
@@ -61,13 +67,9 @@ def fetch_sensor_data():
     return sensor_data
 
 
-def publish_sensor_data(sensor_data, last_sent_time=None):
-    if not last_sent_time:
-        last_sent_time = datetime.fromtimestamp(0)
-
-    data_to_send = sensor_data.ix[sensor_data.index > last_sent_time]
-    for observation_time in data_to_send.index:
-        for sensor in [s for s in data_to_send.columns if s != 'Result']:
+def publish_sensor_data(sensor_data):
+    for observation_time in sensor_data.index:
+        for sensor in [s for s in sensor_data.columns if s != 'Result']:
             message = {
                 'phenomenonTime': str(observation_time),
                 'resultTime': str(datetime.now()),
@@ -77,9 +79,6 @@ def publish_sensor_data(sensor_data, last_sent_time=None):
 
     # block until all messages are sent
     producer.flush()
-
-    # return last sent time
-    return sensor_data.index[-1], len(data_to_send)
 
 
 if __name__ == '__main__':
