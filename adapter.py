cfcc9e9e2c60a960671821e7b27f7a32908e16a1
Johannes Innerbichler <johannes.innerbichler@salzburgresearch.at>
2017-09-28 14:00:43 +0200

    added logstash support

--- adapter.py
+++ adapter.py
@@ -1,24 +1,36 @@
+import os
 import csv
-from datetime import datetime, timedelta
 import json
-import threading
 import logging
-import pandas as pd
+import threading
+from datetime import datetime, timedelta
+import sys
+
 import pytz
 import requests
+import pandas as pd
 from kafka import KafkaProducer
+from logstash import TCPLogstashHandler
 
 CM_APP_HOST = 'http://192.168.13.101'
 KAFKA_TOPIC = 'SensorData'
 BOOTSTRAP_SERVERS = ['il061:9092', 'il062:9092', 'il063:9092']
 UPDATE_INTERVAL = 1  # in minutes
 
+# setup logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+console_logger = logging.StreamHandler(stream=sys.stdout)
+console_logger.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
+logstash_handler = TCPLogstashHandler(host=os.getenv('LOGSTASH_HOST', 'localhost'),
+                                      port=int(os.getenv('LOGSTASH_PORT', 5000)),
+                                      version=1)
+[logger.addHandler(l) for l in [console_logger, logstash_handler]]
+logger.info('Sending logstash to %s:%d', logstash_handler.host, logstash_handler.port)
+
 producer = KafkaProducer(bootstrap_servers=BOOTSTRAP_SERVERS,
                          api_version=(0, 9),
                          value_serializer=lambda m: json.dumps(m).encode('ascii'))
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 
 def update(last_sent_time=None):
@@ -58,7 +70,7 @@ def fetch_sensor_data():
     sensor_data = pd.DataFrame(columns=csv_header)
     for row in csv_reader:
         if row:  # due to blank line at the bottom
-            sensor_data.loc[row[0]] = list(row)  # index is first field (i.e. 'Result')
+            sensor_data.loc[row[0]] = list(row)  # index is first field (i.e. 'Record')
 
     # convert timestamp
     sensor_data['Zeitstempel'] = pd.to_datetime(sensor_data['Zeitstempel'])
@@ -70,11 +82,10 @@ def fetch_sensor_data():
 def publish_sensor_data(sensor_data):
     for observation_time in sensor_data.index:
         for sensor in [s for s in sensor_data.columns if s != 'Record']:
-            message = {
-                'phenomenonTime': observation_time.replace(tzinfo=pytz.UTC).isoformat(),
-                'resultTime': datetime.utcnow().replace(tzinfo=pytz.UTC).isoformat(),
-                'result': float(sensor_data.loc[observation_time, sensor]),
-                'Datastream': {'@iot.id': sensor}}
+            message = {'phenomenonTime': observation_time.replace(tzinfo=pytz.UTC).isoformat(),
+                       'resultTime': datetime.utcnow().replace(tzinfo=pytz.UTC).isoformat(),
+                       'result': float(sensor_data.loc[observation_time, sensor]),
+                       'Datastream': {'@iot.id': sensor}}
             producer.send(KAFKA_TOPIC, message)
 
     # block until all messages are sent
