dece5502773d8db3e49434a91978b8fe7a27afa8
Johannes Innerbichler <johannes.innerbichler@salzburgresearch.at>
2017-09-29 11:52:00 +0200

    added id mapping

--- adapter.py
+++ adapter.py
@@ -15,16 +15,21 @@ from logstash import TCPLogstashHandler
 CM_APP_HOST = 'http://192.168.13.101'
 KAFKA_TOPIC = 'SensorData'
 BOOTSTRAP_SERVERS = ['il061:9092', 'il062:9092', 'il063:9092']
+IGNORED_FIELDS = ['Record', 'Wind', 'Temp_Aussen', 'Feuchte_Aussen']
 UPDATE_INTERVAL = 1  # in minutes
 
+LOGSTASH_HOST = os.getenv('LOGSTASH_HOST', 'localhost')
+LOGSTASH_PORT = int(os.getenv('LOGSTASH_PORT', '5000'))
+
+SENSORTHINGS_HOST = os.getenv('SENSORTHINGS_HOST', 'il060')
+SENSORTHINGS_PORT = os.getenv('SENSORTHINGS_PORT', '8082')
+
 # setup logging
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('cm-adapter')
 logger.setLevel(logging.INFO)
 console_logger = logging.StreamHandler(stream=sys.stdout)
 console_logger.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
-logstash_handler = TCPLogstashHandler(host=os.getenv('LOGSTASH_HOST', 'localhost'),
-                                      port=int(os.getenv('LOGSTASH_PORT', 5000)),
-                                      version=1)
+logstash_handler = TCPLogstashHandler(host=LOGSTASH_HOST, port=LOGSTASH_PORT, version=1)
 [logger.addHandler(l) for l in [console_logger, logstash_handler]]
 logger.info('Sending logstash to %s:%d', logstash_handler.host, logstash_handler.port)
 
@@ -43,7 +48,7 @@ def update(last_sent_time=None):
         data_to_send = sensor_data.ix[sensor_data.index > last_sent_time] if last_sent_time else sensor_data
 
         # delegate to messaging bus
-        publish_sensor_data(sensor_data=data_to_send)
+        publish_sensor_data(sensor_data=data_to_send, id_map=id_mapping, kafka_topic=KAFKA_TOPIC)
         last_sent_time = sensor_data.index[-1]
         logger.info('Published {} new sensor entries till {}'.format(len(data_to_send), last_sent_time))
 
@@ -79,18 +84,39 @@ def fetch_sensor_data():
     return sensor_data
 
 
-def publish_sensor_data(sensor_data):
+def publish_sensor_data(sensor_data, id_map, kafka_topic):
     for observation_time in sensor_data.index:
-        for sensor in [s for s in sensor_data.columns if s != 'Record']:
+        for sensor in [s for s in sensor_data.columns if s not in IGNORED_FIELDS]:
             message = {'phenomenonTime': observation_time.replace(tzinfo=pytz.UTC).isoformat(),
                        'resultTime': datetime.utcnow().replace(tzinfo=pytz.UTC).isoformat(),
                        'result': float(sensor_data.loc[observation_time, sensor]),
-                       'Datastream': {'@iot.id': sensor}}
-            producer.send(KAFKA_TOPIC, message)
+                       'Datastream': {'@iot.id': id_map[sensor]}}
+            producer.send(kafka_topic, message)
 
     # block until all messages are sent
     producer.flush()
 
 
+def map_id(cm_id, mapping):
+    return mapping[cm_id]
+
+
+def fetch_id_mapping(host, port):
+    mapping = dict()
+    url = 'http://{}:{}/v1.0/Datastreams'.format(host, port)
+    while True:
+        url = url.replace('localhost', host).replace('8080', port)  # replace wrong base url and port
+        datastreams = requests.get(url=url).json()
+
+        mapping.update({d['name']: d['@iot.id'] for d in datastreams['value']})
+
+        if '@iot.nextLink' not in datastreams:
+            break
+        url = datastreams['@iot.nextLink']
+
+    return mapping
+
+
 if __name__ == '__main__':
+    id_mapping = fetch_id_mapping(host=SENSORTHINGS_HOST, port=SENSORTHINGS_PORT)
     update()
